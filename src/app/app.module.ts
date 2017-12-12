9a2d29121cf0f05e6d38dd7aa60bfa7f66af17cb
Viktor Sivulskiy <svicvg@gmail.com>
2017-12-12 08:15:21 +0600

    Ionic Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomePage } from '../pages/home/home';
 import { AboutPage } from '../pages/about/about';
 import { MenuPage } from '../pages/menu/menu';
 import { ContactPage } from '../pages/contact/contact';
+import { DishdetailPage } from '../pages/dishdetail/dishdetail';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -24,7 +25,8 @@ import { baseURL } from '../shared/baseurl';
     HomePage,
     AboutPage,
     MenuPage,
-    ContactPage
+    ContactPage,
+    DishdetailPage
   ],
   imports: [
     BrowserModule,
--- src/providers/process-httpmsg/process-httpmsg.ts
+++ src/providers/process-httpmsg/process-httpmsg.ts
@@ -4,7 +4,6 @@ import { Observable } from 'rxjs/Observable';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/throw';
-import 'rxjs/add/operator/catch';
 
 /*
   Generated class for the ProcessHttpmsgProvider provider.
