2be03172e2f2a78d589afa141bf647db5d5f3dee
Viktor Sivulskiy <svicvg@gmail.com>
2017-12-13 11:13:54 +0600

    Advanced Lists

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { DishProvider } from '../../providers/dish/dish';
+import { FavoriteProvider } from '../../providers/favorite/favorite';
 import { DishdetailPage } from '../dishdetail/dishdetail';
 
 /**
@@ -23,6 +24,7 @@ export class MenuPage implements OnInit {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     private dishservice: DishProvider,
+    private favoriteservice: FavoriteProvider,
     @Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
@@ -42,4 +44,9 @@ export class MenuPage implements OnInit {
     });
   }
 
+  addToFavorites(dish: Dish) {
+    console.log('Adding to Favorites', dish.id);
+    this.favoriteservice.addFavorite(dish.id);
+  }
+  
 }
--- src/providers/favorite/favorite.ts
+++ src/providers/favorite/favorite.ts
@@ -1,5 +1,9 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { Dish } from '../../shared/dish';
+import { Observable } from 'rxjs/Observable';
+import { DishProvider } from '../dish/dish';
+import 'rxjs/add/operator/map';
 
 /*
   Generated class for the FavoriteProvider provider.
@@ -12,17 +16,36 @@ export class FavoriteProvider {
   
     favorites: Array<any>;
   
-    constructor(public http: Http) {
+    constructor(public http: Http,
+      private dishservice: DishProvider) {
       console.log('Hello FavoriteProvider Provider');
       this.favorites = [];
     }
   
     addFavorite(id: number): boolean {
+      if (!this.isFavorite(id))
       this.favorites.push(id);
       return true;
     }
   
     isFavorite(id: number): boolean {
           return this.favorites.some(el => el === id);
+    }
+
+    getFavorites(): Observable<Dish[]> {
+      return this.dishservice.getDishes()
+        .map(dishes => dishes.filter(dish => this.favorites.some(el => el === dish.id)));
+    }
+  
+    deleteFavorite(id: number): Observable<Dish[]> {
+      let index = this.favorites.indexOf(id);
+      if (index >= 0) {
+        this.favorites.splice(index,1);
+        return this.getFavorites();
+      }
+      else {
+        console.log('Deleting non-existant favorite', id);
+        return Observable.throw('Deleting non-existant favorite' + id);
+      }
     }
   }
