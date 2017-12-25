c6264fa758405a2d6a538ee199e2a8849ba4dde0
Viktor Sivulskiy <svicvg@gmail.com>
2017-12-25 11:09:02 +0600

    Sending Email

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { LoginPage } from '../pages/login/login';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { LocalNotifications } from '@ionic-native/local-notifications';
+import { EmailComposer } from '@ionic-native/email-composer';
 import { DishProvider } from '../providers/dish/dish';
 import { LeaderProvider } from '../providers/leader/leader';
 import { PromotionProvider } from '../providers/promotion/promotion';
@@ -63,6 +64,7 @@ import { FavoriteProvider } from '../providers/favorite/favorite';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     LocalNotifications,
+    EmailComposer,
     DishProvider,
     LeaderProvider,
     PromotionProvider,
